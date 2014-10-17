9a1151688e99d2b01ab8b59ae7852935d90a753f
Mike McNeil <mike@balderdash.co>
2014-10-16 22:41:59 -0500

    Add --html-directory option.

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ module.exports = {
       return cb( new Error('Please provide a name and/or relative path for this static HTML template (e.g. "benefits-section" or "shared/sidebar"') );
     }
 
+    // Path to html dir within the project
+    // (set using --html-directory)
+    scope.htmlDirectory = scope['html-directory']||'assets/templates';
+
     // Decide the output filename for use in targets below:
     scope.filename = scope.args[0] + '.html';
 
@@ -32,7 +36,7 @@ module.exports = {
 
   targets: {
 
-    './assets/templates/:filename': { template: 'new-html.gen' },
+    './:htmlDirectory/:filename': { template: 'new-html.gen' },
 
   },
 
